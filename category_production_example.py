fea683f9040d75e1b8cf828fc11e29d3a91a6218
Cai <cai@caiwingfield.com>
2018-07-10 10:58:03 +0100

    Output mean response and production frequency correlations

--- category_production_example.py
+++ category_production_example.py
@@ -21,6 +21,7 @@ import json
 from os import path
 
 from pandas import read_csv, DataFrame
+from scipy.stats import spearmanr
 
 from category_production.category_production import CategoryProduction
 from ldm.core.corpus.indexing import FreqDist, TokenIndex
@@ -29,7 +30,7 @@ from ldm.core.utils.maths import DistanceType
 from ldm.preferences.preferences import Preferences as CorpusPreferences
 from model.graph import Graph
 from model.temporal_spreading_activation import TemporalSpreadingActivation, \
-    decay_function_exponential_with_decay_factor, decay_function_gaussian_with_sd_fraction
+    decay_function_exponential_with_decay_factor, decay_function_gaussian_with_sd_fraction, ActivatedNodeEvent
 from model.utils.email import Emailer
 from model.utils.indexing import list_index_dictionaries
 from preferences import Preferences
@@ -42,6 +43,7 @@ logger_dateformat = "%Y-%m-%d %H:%M:%S"
 # Results DataFrame column names
 RESPONSE = "Response"
 NODE_ID = "Node ID"
+ACTIVATION = "Activation"
 TICK_ON_WHICH_ACTIVATED = "Tick on which activated"
 
 
@@ -89,11 +91,11 @@ def main():
     for k, v in node_relabelling_dictionary_json.items():
         node_relabelling_dictionary[int(k)] = v
 
-    category_production = CategoryProduction()
+    cp = CategoryProduction()
 
-    for category_label in category_production.category_labels:
+    # region run model
 
-        # region run model
+    for category_label in cp.category_labels:
 
         # Skip the check if the category won't be in the network
         if category_label not in filtered_words:
@@ -136,6 +138,7 @@ def main():
                 for na in node_activations:
                     model_response_entries.append((
                         na.node,
+                        tsa.label2node[na.node],
                         na.activation,
                         na.tick_activated
                     ))
@@ -149,6 +152,7 @@ def main():
             model_responses_df = DataFrame(model_response_entries, columns=[
                 RESPONSE,
                 NODE_ID,
+                ACTIVATION,
                 TICK_ON_WHICH_ACTIVATED
             ])
 
@@ -162,29 +166,59 @@ def main():
                 model_responses_df.to_csv(output_file, index=False)
 
         else:
-            logger.info(f"{model_responses_path} exists, loading prior results.")
-            model_responses_df = read_csv(model_responses_path, header=0, comments="#", index=False)
+            logger.info(f"{model_responses_path} exists, skipping.")
 
-        model_responses = set(model_responses_df["Response"])
+    # endregion
 
-        # endregion
+    # region compare model with data
 
-        # region compare model with data
+    for category_label in cp.category_labels:
 
-        response_overlap = [mr
-                            for mr in model_responses
-                            if mr in category_production.responses_for_category(category_label, single_word_only=True)]
+        # Skip the check if the category won't be in the network
+        if category_label not in filtered_words:
+            continue
 
-        coverage_percent = 100 * len(set(response_overlap)) / len(set(category_production.responses_for_category(category_label, single_word_only=True)))
+        # Dictionary of differently-ordered lists of words
+        actual_response_words_by_mean_rank = [r
+                                              for r in cp.responses_for_category(category_label, single_word_only=True, sort_by=CategoryProduction.ColNames.MeanRank)
+                                              if r in filtered_words]
+        n_actual_response_words = len(actual_response_words_by_mean_rank)
 
-        model_entries = []
-        for row in model_responses_df.iterrows():
-            model_response = row[RESPONSE]
-            tick = row[TICK_ON_WHICH_ACTIVATED]
-            model_entries.append((model_response, tick))
+        # Load model responses
+        model_responses_path = path.join(Preferences.output_dir, f"Category production traces ({n_words:,} words)", f"responses_{category_label}_{n_words:,}.csv")
+        model_responses_df = read_csv(model_responses_path, header=0, comments="#", index=False)
+        model_response_entries = []
+        for row in model_responses_df.sort_values(by=TICK_ON_WHICH_ACTIVATED).iterrows():
+            model_response_entries.append(ActivatedNodeEvent(
+                node=row[RESPONSE], activation=row[ACTIVATION], tick_activated=row[TICK_ON_WHICH_ACTIVATED]))
 
-        production_frequency_vector_for_overlap = []
+        # Get overlap
+        model_response_overlap_entries = []
+        for mr in model_response_entries:
+            # Only interested in overlap
+            if mr.node not in actual_response_words_by_mean_rank:
+                continue
+            # Only interested in unique entries
+            if mr.node in [existing_mr.node for existing_mr in model_response_overlap_entries]:
+                continue
+
+        coverage_percent = 100 * len(set(model_response_overlap_entries)) / n_actual_response_words
+
+        # Comparison vectors
+
+        # model response vector will contain ticks on which the entry was (first) activated
         model_response_vector_for_overlap = []
+        # production frequency vector will contain the production frequency
+        production_frequency_vector_for_overlap = []
+        # mean rank vector will contain mean ranks
+        mean_rank_vector_for_overlap = []
+        for common_entry in model_response_overlap_entries:
+            model_response_vector_for_overlap.append(common_entry.tick_activated)
+            mean_rank_vector_for_overlap.append(cp.data_for_category_response_pair(category_label, common_entry.node, CategoryProduction.ColNames.MeanRank))
+            production_frequency_vector_for_overlap.append(cp.data_for_category_response_pair(category_label, common_entry.node, CategoryProduction.ColNames.ProductionFrequency))
+
+        mean_rank_corr = spearmanr(model_response_vector_for_overlap, model_response_vector_for_overlap).rs
+        production_frequency_corr = spearmanr(model_response_vector_for_overlap, production_frequency_vector_for_overlap).rs
 
         # endregion
 
@@ -195,32 +229,26 @@ def main():
         with open(model_effectiveness_path, mode="w", encoding="utf-8") as model_efficacy_file:
 
             model_efficacy_file.write("Response overlap:\n")
-            model_efficacy_file.write("\t" + "\n\t".join(response_overlap) + "\n")
+            model_efficacy_file.write("\t" + "\n\t".join([mr.node for mr in model_response_overlap_entries]) + "\n")
             model_efficacy_file.write("\n")
 
             model_efficacy_file.write("Coverage: what percentage of human-listed responses are found by the model in the first 1000 ticks or before bailout?\n")
             model_efficacy_file.write("{percent:.2f}%\n".format(percent=coverage_percent))
             model_efficacy_file.write("\n")
 
-            model_efficacy_file.write("Mean rank: where do model responses lie in order of actual responses?\n")
-            model_efficacy_file.write("\t" + "\n\t".join([str(
-                category_production.responses_for_category(category_label,
-                                                           single_word_only=True,
-                                                           sort_by=CategoryProduction.ColNames.MeanRank).index(r))
-                                                          for r
-                                                          in response_overlap]) + "\n")
+            model_efficacy_file.write("Mean rank correlation:\n")
+            model_efficacy_file.write(f"\t{mean_rank_corr}")
             model_efficacy_file.write("\n")
 
-            model_efficacy_file.write("Actual responses (ordered by Mean Rank):\n")
-            model_efficacy_file.write("\t" + "\n\t".join(category_production.responses_for_category(
-                category_label, single_word_only=True, sort_by=CategoryProduction.ColNames.MeanRank)) + "\n")
+            model_efficacy_file.write("Production frequency correlation:\n")
+            model_efficacy_file.write(f"\t{production_frequency_corr}")
             model_efficacy_file.write("\n")
 
-            model_efficacy_file.write("Model responses (ordered by activation order, random order amongst ties):\n")
-            model_efficacy_file.write("\t" + "\n\t".join(model_response_entries) + "\n")
+            model_efficacy_file.write("Actual responses (those present in corpus, ordered by Mean Rank):\n")
+            model_efficacy_file.write("\t" + "\n\t".join(actual_response_words_by_mean_rank) + "\n")
             model_efficacy_file.write("\n")
 
-        # endregion
+    # endregion
 
     emailer = Emailer(Preferences.email_connection_details_path)
     emailer.send_email(f"Done running {path.basename(__file__)} with {n_words} words.", Preferences.target_email_address)
