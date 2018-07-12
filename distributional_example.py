4c603925885daa91c85f019a60ea49b7047973e0
Cai <cai@caiwingfield.com>
2018-07-12 13:09:10 +0100

    rename activation_threshold -> firing_threshold

--- distributional_example.py
+++ distributional_example.py
@@ -97,16 +97,16 @@ def main():
 
     # Run spreading activation
     d = []
-    for activation_threshold in [0.8]:
+    for firing_threshold in [0.8]:
         for node_decay_factor in [0.99]:
             for edge_decay_sd in [400]:
 
                 logger.info(f"Setting up spreading output")
-                logger.info(f"Using values: l={length_factor}, θ={activation_threshold}, δ={node_decay_factor}, sd={edge_decay_sd}")
+                logger.info(f"Using values: l={length_factor}, θ={firing_threshold}, δ={node_decay_factor}, sd={edge_decay_sd}")
 
                 tsa = TemporalSpreadingActivation(
                     graph=graph,
-                    activation_threshold=activation_threshold,
+                    firing_threshold=firing_threshold,
                     impulse_pruning_threshold=impulse_pruning_threshold,
                     node_relabelling_dictionary=node_relabelling_dictionary,
                     node_decay_function=decay_function_exponential_with_decay_factor(
@@ -130,7 +130,7 @@ def main():
                     d.append({
                         'Tick': tick,
                         'Nodes fired': nodes_fired_str,
-                        "Activation threshold": activation_threshold,
+                        "Activation threshold": firing_threshold,
                         "Node decay factor": node_decay_factor,
                         "Edge decay SD": edge_decay_sd,
                         "Activated nodes": tsa.n_suprathreshold_nodes()
