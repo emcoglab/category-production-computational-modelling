5bdedc67ca46ffba422709b4d9bab36a6bb705ba
Cai <c.wingfield@lancaster.ac.uk>
2018-08-16 16:07:46 +0100

    Check connectedness of pruned graphs.

--- connectedness_of_pruned_graphs.py
+++ connectedness_of_pruned_graphs.py
@@ -1,6 +1,6 @@
 """
 ===========================
-Finds the smallest pruning threshold estimate for a graph.
+Check the connectedness of pruned graphs.
 ===========================
 
 Dr. Cai Wingfield
@@ -16,16 +16,16 @@ caiwingfield.net
 """
 import logging
 import sys
-from collections import defaultdict
 from os import path
 
-import numpy
+from matplotlib import pyplot
+from seaborn import distplot
 
 from ldm.core.corpus.indexing import FreqDist
 from ldm.core.model.count import LogCoOccurrenceCountModel
 from ldm.core.utils.maths import DistanceType
 from ldm.preferences.preferences import Preferences as CorpusPreferences
-from model.graph import edge_data_from_edgelist
+from model.graph import Graph
 from preferences import Preferences
 
 logger = logging.getLogger(__name__)
@@ -33,7 +33,7 @@ logger_format = '%(asctime)s | %(levelname)s | %(module)s | %(message)s'
 logger_dateformat = "%Y-%m-%d %H:%M:%S"
 
 NODE_COUNTS = [
-    # 1_000,
+    1_000,
     # 3_000,
     # 5_000,
     # 10_000,
@@ -42,9 +42,22 @@ NODE_COUNTS = [
     # 25_000,
     # 30_000,
     # 35_000,
-    40_000,
+    # 40_000,
 ]
 LENGTH_FACTOR = 1_000
+TOP_QUANTILES = [
+    0.0,
+    0.1,
+    0.2,
+    0.3,
+    0.4,
+    0.5,
+    0.6,
+    0.7,
+    0.8,
+    0.9,
+    1.0,
+]
 
 
 def main():
@@ -59,21 +72,34 @@ def main():
 
         # Load the full graph
         graph_file_name = f"{distributional_model.name} {distance_type.name} {n_words} words length {LENGTH_FACTOR}.edgelist"
+        logger.info(f"Loading graph from {graph_file_name}")
+        graph = Graph.load_from_edgelist(path.join(Preferences.graphs_dir, graph_file_name))
+        logger.info(f"Graph has {len(graph.edges):,} edges")
 
-        min_edge_lengths = defaultdict(lambda: numpy.inf)
+        f = pyplot.figure()
+        distplot([graph.edge_data[edge].length for edge in graph.edges])
+        f.savefig("/Users/caiwingfield/Desktop/fig.png")
 
-        for edge, edge_data in edge_data_from_edgelist(path.join(Preferences.graphs_dir, graph_file_name)):
-            n1, n2 = edge.nodes
-            length = edge_data.length
-            min_edge_lengths[n1] = min(min_edge_lengths[n1], length)
-            min_edge_lengths[n2] = min(min_edge_lengths[n2], length)
+        # Prune by percentage
+        # Invert the quantiles so q of 0.1 gives TOP 10%
+        pruning_lengths = [graph.edge_length_quantile(1-q) for q in TOP_QUANTILES]
 
-        max_min_length = -numpy.inf
-        for node, min_length in min_edge_lengths.items():
-            max_min_length = max(max_min_length, min_length)
-
-        # Display the max of those
-        logger.info(f"Max min-incident-edge length for {n_words:,}-node graph with length factor {LENGTH_FACTOR:,}: {max_min_length}")
+        for i, pruning_length in enumerate(pruning_lengths):
+            top_quantile = TOP_QUANTILES[i]
+            logger.info(f"Pruning longest {int(100*top_quantile)}% of edges (anything longer than {pruning_length}).")
+            graph.prune_longest_edges_by_quantile(top_quantile)
+            logger.info(f"Graph has {len(graph.edges):,} edges")
+            if graph.has_orphaned_nodes():
+                logger.info("Graph has orphaned nodes")
+            else:
+                logger.info("Graph has no orphaned nodes")
+            if graph.is_connected():
+                logger.info("Graph is connected")
+            else:
+                logger.info("Graph is disconnected")
+                break
+            logger.info("")
+        logger.info("")
 
 
 if __name__ == '__main__':
