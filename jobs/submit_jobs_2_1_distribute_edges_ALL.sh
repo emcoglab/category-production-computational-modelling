ff56d2c64983954f6ab7729135bd006351ed839b
Cai <c.wingfield@lancaster.ac.uk>
2018-08-28 14:22:42 +0100

    We can’t do edge redistribution (pseudo-undo 2d9948de)
    Too much IO for the HEC.

--- jobs/submit_jobs_2_1_distribute_edges_ALL.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-qsub job_2_1_distribute_edges_1k.sh
-qsub job_2_1_distribute_edges_3k.sh
-qsub job_2_1_distribute_edges_5k.sh
-qsub job_2_1_distribute_edges_10k.sh
-qsub job_2_1_distribute_edges_15k.sh
-qsub job_2_1_distribute_edges_20k.sh
-qsub job_2_1_distribute_edges_30k.sh
