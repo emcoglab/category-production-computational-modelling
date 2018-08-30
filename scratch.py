b242a2d953718f5184ceecaad72c92adf0ac15a9
Cai <c.wingfield@lancaster.ac.uk>
2018-08-30 15:35:46 +0100

    Class representing a set of points in space

--- scratch.py
+++ scratch.py
@@ -1,35 +1,10 @@
-from os import path
+import numpy
 
-from pandas import DataFrame
+from model.temporal_spatial_expansion import PointsInSpace, Point
 
-from ldm.core.corpus.indexing import FreqDist, TokenIndex
-from ldm.core.model.count import LogCoOccurrenceCountModel
-from ldm.core.utils.maths import DistanceType
-from ldm.preferences.preferences import Preferences as CorpusPreferences
-from model.graph import Graph
-from preferences import Preferences
+p1 = Point(numpy.array([1, 2, 3]), "p1")
+p2 = Point(numpy.array([4, 5, 6]), "p2")
 
-length_factor = 1_000
-n_words = 1_000
-prune_percent = 50
+pis = PointsInSpace(numpy.vstack((p1.vector, p2.vector)), {i: p.label for i, p in enumerate([p1, p2])})
 
-corpus = CorpusPreferences.source_corpus_metas.bbc
-distance_type = DistanceType.cosine
-freq_dist = FreqDist.load(corpus.freq_dist_path)
-token_index = TokenIndex.from_freqdist_ranks(freq_dist)
-distributional_model = LogCoOccurrenceCountModel(corpus, window_radius=5, freq_dist=freq_dist)
-
-graph_file_name = f"{distributional_model.name} {distance_type.name} {n_words} words length {length_factor}.edgelist"
-quantile_file_name = f"{distributional_model.name} {distance_type.name} {n_words} words length {length_factor} edge length quantiles.csv"
-
-quantile_data = DataFrame.from_csv(path.join(Preferences.graphs_dir, quantile_file_name), header=0, index_col=None)
-pruning_length = quantile_data[
-    # Use 1 - so that smallest top quantiles get converted to longest edges
-    quantile_data["Top quantile"] == 1 - (prune_percent / 100)
-    ]["Pruning length"].iloc[0]
-
-graph = Graph.load_from_edgelist(file_path=path.join(Preferences.graphs_dir, graph_file_name),
-                                 ignore_edges_longer_than=pruning_length,
-                                 keep_at_least_n_edges=Preferences.min_edges_per_node)
-
-print("Done")
+pass
---
+++ tests/test_points_in_space.py
@@ -0,0 +1,39 @@
+"""
+===========================
+Tests for graph functions.
+===========================
+
+Dr. Cai Wingfield
+---------------------------
+Embodied Cognition Lab
+Department of Psychology
+University of Lancaster
+c.wingfield@lancaster.ac.uk
+caiwingfield.net
+---------------------------
+2018
+---------------------------
+"""
+
+import unittest
+
+from numpy import array, vstack
+
+from model.temporal_spatial_expansion import Point, PointsInSpace
+
+
+class TestGraphPruning(unittest.TestCase):
+
+    def test_dimensions(self):
+
+        p1 = Point(array([1, 2, 3]), "p1")
+        p2 = Point(array([4, 5, 6]), "p2")
+
+        pis = PointsInSpace(vstack((p1.vector, p2.vector)), {i: p.label for i, p in enumerate([p1, p2])})
+
+        self.assertEqual(pis.n_points, 2)
+        self.assertEqual(pis.n_dims, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
