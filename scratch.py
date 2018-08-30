2993f7a37e0905eb990337e9d82f025741e95383
Cai <c.wingfield@lancaster.ac.uk>
2018-08-30 15:45:12 +0100

    Sensorimotor norms presented as points in space.

--- scratch.py
+++ scratch.py
@@ -1,10 +1,8 @@
 import numpy
 
+from model.sensorimotor import SensorimotorNorms
 from model.temporal_spatial_expansion import PointsInSpace, Point
 
-p1 = Point(numpy.array([1, 2, 3]), "p1")
-p2 = Point(numpy.array([4, 5, 6]), "p2")
-
-pis = PointsInSpace(numpy.vstack((p1.vector, p2.vector)), {i: p.label for i, p in enumerate([p1, p2])})
+pis: PointsInSpace = SensorimotorNorms().as_points_in_space()
 
 pass
