784238ba92125db12a640ec20e906bcfc83b26a4
Cai <c.wingfield@lancaster.ac.uk>
2018-09-06 15:45:24 +0100

    Flesh out class and use lognormal tail function as decay.

--- scratch.py
+++ scratch.py
@@ -1,6 +1,13 @@
-from model.sensorimotor import SensorimotorNorms
-from model.points_in_space import PointsInSpace
+from matplotlib import pyplot
+from numpy import linspace, log, exp
+from scipy.stats import lognorm, norm
 
-pis: PointsInSpace = SensorimotorNorms().as_points_in_space()
-
-pass
+x = linspace(0, 10, 1000)
+sigma = 0.35
+median = 3.75
+mu = log(median)
+y = 1-lognorm.cdf(x, s=sigma, scale=median)
+y = lognorm.sf(x, s=sigma, scale=median)
+# y = 1-norm.cdf(x, loc=mu, scale=sigma)
+pyplot.plot(x, y)
+pyplot.show()
