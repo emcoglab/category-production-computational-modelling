c139a57406201dc2e5fa3a41ebed53be420d549d
Cai <c.wingfield@lancaster.ac.uk>
2018-09-25 12:33:58 +0100

    Compile dataframe and compute stats from there

--- 3_category_production_comparison.py
+++ 3_category_production_comparison.py
@@ -22,13 +22,16 @@ import glob
 import logging
 import re
 import sys
+from collections import defaultdict
 from os import path
+from typing import Dict, DefaultDict
 
-from numpy import nan, mean, nanmean
+from numpy import nan
 from pandas import read_csv, DataFrame
-from scipy.stats import spearmanr, pearsonr, sem
+from scipy.stats import spearmanr
 
 from category_production.category_production import CategoryProduction
+from category_production.category_production import ColNames as CPColNames
 from ldm.core.corpus.indexing import FreqDist
 from ldm.preferences.preferences import Preferences as CorpusPreferences
 from model.component import ItemActivatedEvent
@@ -46,6 +49,7 @@ RESPONSE = "Response"
 NODE_ID = "Node ID"
 ACTIVATION = "Activation"
 TICK_ON_WHICH_ACTIVATED = "Tick on which activated"
+TTFA = "TTFA"
 
 MIN_FIRST_RANK_FREQ = 4
 
@@ -62,8 +66,6 @@ def main_in_path(results_dir: str):
 
     # Vectors for statistics of interest, to be computed over all categories
 
-    # Average RTs for all category–first-rank-member pairs (with FRF ≥ `min_first_rank_freq`)
-    first_rank_mean_zrts = []
     # Corresponding time-to-activation of member nodes from category seed
     first_rank_tta = []
 
@@ -77,6 +79,46 @@ def main_in_path(results_dir: str):
     # per-category stats
 
     corrs_mean_rank_vs_tta = []
+
+    # Main dataframe holds category production data and model response data
+    main_dataframe: DataFrame = cp.data.copy()
+
+    # Drop precomputed distance measures
+    main_dataframe.drop(['LgSUBTLWF', 'Sensorimotor', 'Linguistic'], axis=1, inplace=True)
+
+    # Add model TTFA column
+    model_ttfas: Dict[str, DefaultDict[str, int]] = dict()   # category -> response -> TTFA
+    for category in cp.category_labels:
+        model_ttfas[category] = get_model_ttfas_for_category(category, results_dir, n_words)
+    main_dataframe[TTFA] = main_dataframe.apply(lambda row: model_ttfas[row[CPColNames.Category]][row[CPColNames.Response]], axis=1)
+
+    # Drop rows corresponding to responses which weren't produced by the model
+    main_dataframe = main_dataframe[main_dataframe[TTFA].notnull()]
+
+    # Compute overall stats
+
+    # The Pearson's correlation over all categories between average first-response RT (for responses with
+    # first-rank frequency ≥4) and the time to the first activation (TTFA) within the model.
+
+    # First rank frequency cutoff
+    first_rank_frequent_data = main_dataframe[main_dataframe[CPColNames.FirstRankFrequency] >= MIN_FIRST_RANK_FREQ]
+    n_first_rank_frequent = first_rank_frequent_data.shape[0]
+
+    first_rank_frequent_corr_rt_vs_ttfa = first_rank_frequent_data[CPColNames.MeanZRT].corr(first_rank_frequent_data[TTFA], method='pearson')
+
+    # The average (over categories) Spearman's correlation (over responses) between the mean rank of the response and
+    # the time to first activation in the model.
+
+    correlations_per_category = (
+        main_dataframe
+        .groupby(CPColNames.Category)
+        # Correlate MeanRank and TTFA
+        [[CPColNames.MeanRank, TTFA]].corr(method='spearman')
+        # .corr gives the 2x2 correlation matrix for these two variables, but we just want the off-diagonal entries
+        .iloc[0::2, -1])
+
+    average_corr_meanrank_vs_ttfa = correlations_per_category.mean()
+    sem_corr_meanrank_vs_ttfa = correlations_per_category.sem()
 
     for category_label in cp.category_labels:
 
@@ -114,10 +156,10 @@ def main_in_path(results_dir: str):
         model_response_overlap_entries = []
         for model_response in model_response_entries:
             # Only interested in overlap
-            if model_response.node not in actual_response_words:
+            if model_response.label not in actual_response_words:
                 continue
             # Only interested in first among repeated entries
-            if model_response.node in [existing_mr.node for existing_mr in model_response_overlap_entries]:
+            if model_response.label in [existing_mr.label for existing_mr in model_response_overlap_entries]:
                 continue
             model_response_overlap_entries.append(model_response)
 
@@ -125,17 +167,17 @@ def main_in_path(results_dir: str):
 
         # model response vector will contain ticks on which the entry was (first) activated
         times_to_first_activation_in_model = [
-            common_entry.tick_activated
+            common_entry.time_activated
             for common_entry in model_response_overlap_entries
         ]
         # production frequency vector will contain the production frequency
         production_frequencies_in_data = [
-            cp.data_for_category_response_pair(category_label, common_entry.node, CategoryProduction.ColNames.ProductionFrequency)
+            cp.data_for_category_response_pair(category_label, common_entry.label, CPColNames.ProductionFrequency)
             for common_entry in model_response_overlap_entries
         ]
         # mean rank vector will contain mean ranks
         mean_ranks = [
-            cp.data_for_category_response_pair(category_label, common_entry.node, CategoryProduction.ColNames.MeanRank)
+            cp.data_for_category_response_pair(category_label, common_entry.label, CPColNames.MeanRank)
             for common_entry in model_response_overlap_entries
         ]
 
@@ -145,18 +187,11 @@ def main_in_path(results_dir: str):
 
         mean_response_ranks_data.extend(mean_ranks)
         time_to_activation_model.extend(times_to_first_activation_in_model)
-        # First rank frequency RTs
-        first_rank_mean_zrts.extend([
-            mean(list(cp.rts_for_category_response_pair(category_label, response.node, use_zrt=True)))
-            for response in model_response_overlap_entries
-            # but only those above threshold
-            if cp.data_for_category_response_pair(category_label, response.node, CategoryProduction.ColNames.FirstRankFrequency) >= MIN_FIRST_RANK_FREQ
-        ])
         first_rank_tta.extend([
             response.tick_activated
             for response in model_response_overlap_entries
             # but only those above threshold
-            if cp.data_for_category_response_pair(category_label, response.node, CategoryProduction.ColNames.FirstRankFrequency) >= MIN_FIRST_RANK_FREQ
+            if cp.data_for_category_response_pair(category_label, response.label, CPColNames.FirstRankFrequency) >= MIN_FIRST_RANK_FREQ
         ])
 
         # Compute statistics for this category
@@ -174,17 +209,13 @@ def main_in_path(results_dir: str):
             100 * corpus_coverage_this_category,
             len(model_response_overlap_entries),
             100 * len(model_response_overlap_entries) / len(actual_response_words) if len(actual_response_words) > 0 else nan,
-            str([e.node for e in model_response_overlap_entries]),
+            str([e.label for e in model_response_overlap_entries]),
             str(times_to_first_activation_in_model),
             str(mean_ranks),
             corr_mean_rank_vs_time_to_activation,
             str(production_frequencies_in_data),
             corr_production_freq_vs_time_to_activation,
         ))
-
-    first_rank_rt_corr, _ = pearsonr(first_rank_mean_zrts, first_rank_tta)
-    mean_rank_corr = nanmean(corrs_mean_rank_vs_tta)
-    sem_rank_corr = sem(corrs_mean_rank_vs_tta, nan_policy='omit')
 
     # Paths
     per_category_stats_output_path = path.join(Preferences.results_dir, "Category production fit", f"model_effectiveness_per_category ({path.basename(results_dir)}).csv")
@@ -215,11 +246,11 @@ def main_in_path(results_dir: str):
         output_file.write(f"First response RT vs TTA correlation ("
                           f"Pearson's; positive is better fit; "
                           f"FRF≥{MIN_FIRST_RANK_FREQ}; "
-                          f"N = {len(first_rank_mean_zrts)}) "
-                          f"= {first_rank_rt_corr}\n")
+                          f"N = {n_first_rank_frequent}) "
+                          f"= {first_rank_frequent_corr_rt_vs_ttfa}\n")
         output_file.write(f"Average mean_rank vs time-to-activation correlation ("
                           f"Spearman's; positive is better fit) "
-                          f"= {mean_rank_corr} (SEM = {sem_rank_corr})\n")
+                          f"= {average_corr_meanrank_vs_ttfa} (SEM = {sem_corr_meanrank_vs_ttfa})\n")
 
 
 def interpret_path(results_dir_path: str) -> int:
@@ -264,6 +295,41 @@ def interpret_path(results_dir_path: str) -> int:
         raise ParseError(f"Could not parse \"{dir_name}\"")
 
     return n_words
+
+
+def get_model_ttfas_for_category(category: str, results_dir: str, n_words: int) -> DefaultDict[str, int]:
+    """
+    Dictionary of
+        response -> time to first activation
+    for the specified category.
+
+    DefaultDict gives nans where response not found
+
+    :param category:
+    :param results_dir:
+    :param n_words:
+    :return:
+    """
+
+    # Try to load model response
+    try:
+        model_responses_path = path.join(
+            results_dir,
+            f"responses_{category}_{n_words:,}.csv")
+        with open(model_responses_path, mode="r", encoding="utf-8") as model_responses_file:
+            model_responses_df: DataFrame = read_csv(model_responses_file, header=0, comment="#", index_col=False)
+
+        ttfas = defaultdict(lambda: nan)
+        for row_i, row in model_responses_df.sort_values(by=TICK_ON_WHICH_ACTIVATED).iterrows():
+            activation_event = ItemActivatedEvent(label=row[RESPONSE], activation=row[ACTIVATION], time_activated=row[TICK_ON_WHICH_ACTIVATED])
+            # We've sorted by activation time, so we only need to consider the first entry for each item
+            if activation_event.label not in ttfas.keys():
+                ttfas[activation_event.label] = activation_event.time_activated
+        return ttfas
+
+    # If the category wasn't found, there are no TTFAs
+    except FileNotFoundError:
+        return defaultdict(lambda: nan)
 
 
 if __name__ == '__main__':
