18252b8ca4f370b4142d4314e4a1ab13dab34efc
Cai <c.wingfield@lancaster.ac.uk>
2018-09-25 12:56:00 +0100

    Save dataframe rather than outputting convoluted lists

--- 3_category_production_comparison.py
+++ 3_category_production_comparison.py
@@ -27,13 +27,10 @@ from os import path
 from typing import Dict, DefaultDict
 
 from numpy import nan
-from pandas import read_csv, DataFrame, to_numeric
-from scipy.stats import spearmanr
+from pandas import read_csv, DataFrame
 
 from category_production.category_production import CategoryProduction
 from category_production.category_production import ColNames as CPColNames
-from ldm.core.corpus.indexing import FreqDist
-from ldm.preferences.preferences import Preferences as CorpusPreferences
 from model.component import ItemActivatedEvent
 from model.utils.exceptions import ParseError
 from preferences import Preferences
@@ -54,40 +51,17 @@ TTFA = "TTFA"
 MIN_FIRST_RANK_FREQ = 4
 
 
-def main_in_path(results_dir: str):
+def main_in_path(results_dir: str, category_production: CategoryProduction):
     n_words = interpret_path(results_dir)
 
-    # Load main data
-
-    corpus = CorpusPreferences.source_corpus_metas.bbc
-    freq_dist = FreqDist.load(corpus.freq_dist_path)
-    cp = CategoryProduction()
-    linguistic_model_vocab = set(freq_dist.most_common_tokens(n_words))
-
-    # Vectors for statistics of interest, to be computed over all categories
-
-    # Corresponding time-to-activation of member nodes from category seed
-    first_rank_tta = []
-
-    # Of responses which were produced by both model and in the data, what are the ordinals of the (first occurrences of
-    #  the) responses?
-    mean_response_ranks_data = []
-    time_to_activation_model = []
-
-    category_comparisons = []
-
-    # per-category stats
-
-    corrs_mean_rank_vs_tta = []
-
     # Main dataframe holds category production data and model response data
-    main_dataframe: DataFrame = cp.data.copy()
+    main_dataframe: DataFrame = category_production.data.copy()
     # Drop precomputed distance measures
     main_dataframe.drop(['LgSUBTLWF', 'Sensorimotor', 'Linguistic'], axis=1, inplace=True)
 
     # Add model TTFA column
     model_ttfas: Dict[str, DefaultDict[str, int]] = dict()   # category -> response -> TTFA
-    for category in cp.category_labels:
+    for category in category_production.category_labels:
         model_ttfas[category] = get_model_ttfas_for_category(category, results_dir, n_words)
     main_dataframe[TTFA] = main_dataframe.apply(lambda row: model_ttfas[row[CPColNames.Category]][row[CPColNames.Response]], axis=1)
 
@@ -96,6 +70,11 @@ def main_in_path(results_dir: str):
 
     # Now we can convert TTFAs to ints as there won't be null values
     main_dataframe[TTFA] = main_dataframe[TTFA].astype(int)
+
+    # Save main dataframe
+
+    per_category_stats_output_path = path.join(Preferences.results_dir, "Category production fit", f"model_effectiveness_per_category ({path.basename(results_dir)}).csv")
+    main_dataframe.to_csv(per_category_stats_output_path, index=False)
 
     # Compute overall stats
 
@@ -122,126 +101,8 @@ def main_in_path(results_dir: str):
     average_corr_meanrank_vs_ttfa = correlations_per_category.mean()
     sem_corr_meanrank_vs_ttfa = correlations_per_category.sem()
 
-    for category_label in cp.category_labels:
-
-        # Skip the check if the category won't be in the network
-        if category_label not in linguistic_model_vocab:
-            continue
-
-        # Load MODEL responses
-        try:
-            model_responses_path = path.join(
-                results_dir,
-                f"responses_{category_label}_{n_words:,}.csv")
-            with open(model_responses_path, mode="r", encoding="utf-8") as model_responses_file:
-                model_responses_df = read_csv(model_responses_file, header=0, comment="#", index_col=False)
-
-        # Skip any we don't have yet
-        except FileNotFoundError as e:
-            logger.warning(f"File not found: {e.filename}")
-            continue
-
-        # Collect ACTUAL responses
-
-        # Dictionary of differently-ordered lists of words
-        actual_response_words = [response
-                                 for response in cp.responses_for_category(category_label, single_word_only=True)
-                                 if response in linguistic_model_vocab]
-        corpus_coverage_this_category = len(actual_response_words) / len(cp.responses_for_category(category_label, single_word_only=True))
-
-        model_response_entries = [
-            ItemActivatedEvent(label=row[RESPONSE], activation=row[ACTIVATION], time_activated=row[TICK_ON_WHICH_ACTIVATED])
-            for row_i, row in model_responses_df.sort_values(by=TICK_ON_WHICH_ACTIVATED).iterrows()
-        ]
-
-        # Get overlap
-        model_response_overlap_entries = []
-        for model_response in model_response_entries:
-            # Only interested in overlap
-            if model_response.label not in actual_response_words:
-                continue
-            # Only interested in first among repeated entries
-            if model_response.label in [existing_mr.label for existing_mr in model_response_overlap_entries]:
-                continue
-            model_response_overlap_entries.append(model_response)
-
-        # Model–data comparisons vectors for individual categories
-
-        # model response vector will contain ticks on which the entry was (first) activated
-        times_to_first_activation_in_model = [
-            common_entry.time_activated
-            for common_entry in model_response_overlap_entries
-        ]
-        # production frequency vector will contain the production frequency
-        production_frequencies_in_data = [
-            cp.data_for_category_response_pair(category_label, common_entry.label, CPColNames.ProductionFrequency)
-            for common_entry in model_response_overlap_entries
-        ]
-        # mean rank vector will contain mean ranks
-        mean_ranks = [
-            cp.data_for_category_response_pair(category_label, common_entry.label, CPColNames.MeanRank)
-            for common_entry in model_response_overlap_entries
-        ]
-
-        # Comparisons over all categories
-
-        # TODO: this stuff would be better done by building one large dataframe and then just doing the stats on that
-
-        mean_response_ranks_data.extend(mean_ranks)
-        time_to_activation_model.extend(times_to_first_activation_in_model)
-        first_rank_tta.extend([
-            response.tick_activated
-            for response in model_response_overlap_entries
-            # but only those above threshold
-            if cp.data_for_category_response_pair(category_label, response.label, CPColNames.FirstRankFrequency) >= MIN_FIRST_RANK_FREQ
-        ])
-
-        # Compute statistics for this category
-
-        # noinspection PyTypeChecker
-        corr_mean_rank_vs_time_to_activation, _ = spearmanr(times_to_first_activation_in_model, mean_ranks)
-        corrs_mean_rank_vs_tta.append(corr_mean_rank_vs_time_to_activation)
-        # noinspection PyTypeChecker
-        corr_production_freq_vs_time_to_activation, _ = spearmanr(times_to_first_activation_in_model, production_frequencies_in_data)
-
-        category_comparisons.append((
-            n_words,
-            category_label,
-            len(actual_response_words),
-            100 * corpus_coverage_this_category,
-            len(model_response_overlap_entries),
-            100 * len(model_response_overlap_entries) / len(actual_response_words) if len(actual_response_words) > 0 else nan,
-            str([e.label for e in model_response_overlap_entries]),
-            str(times_to_first_activation_in_model),
-            str(mean_ranks),
-            corr_mean_rank_vs_time_to_activation,
-            str(production_frequencies_in_data),
-            corr_production_freq_vs_time_to_activation,
-        ))
-
-    # Paths
-    per_category_stats_output_path = path.join(Preferences.results_dir, "Category production fit", f"model_effectiveness_per_category ({path.basename(results_dir)}).csv")
-    overall_stats_output_path      = path.join(Preferences.results_dir, "Category production fit", f"model_effectiveness_overall ({path.basename(results_dir)}).txt")
-
-    # Save per-category stats
-    category_comparisons_df = DataFrame(category_comparisons, columns=[
-        f"Number of words",
-        f"Category",
-        f"Actual responses in corpus",
-        f"Corpus coverage of actual responses (%)",
-        f"Model response overlap size",
-        f"Model response overlap %",
-        f"Model response overlap words",
-        f"Model: time to first activation",
-        f"Mean ranks",
-        f"Mean rank correlation (Spearman's; positive is better fit)",
-        f"Production frequencies",
-        f"Production frequency correlation (Spearman's; negative is better fit)",
-    ])
-    with open(per_category_stats_output_path, mode="w", encoding="utf-8") as output_file:
-        category_comparisons_df.to_csv(output_file, index=False)
-
     # Save overall stats
+    overall_stats_output_path      = path.join(Preferences.results_dir, "Category production fit", f"model_effectiveness_overall ({path.basename(results_dir)}).txt")
     with open(overall_stats_output_path, mode="w", encoding="utf-8") as output_file:
         # Correlation of first response RT with time-to-activation
         output_file.write(path.basename(results_dir) + "\n\n")
@@ -347,9 +208,11 @@ if __name__ == '__main__':
 
     dirs = glob.glob(path.join(args.path, "*"))
 
+    cp = CategoryProduction()
+
     for d in dirs:
         if path.isdir(d):
             logger.info(path.basename(d))
-            main_in_path(d)
+            main_in_path(d, cp)
 
     logger.info("Done!")
