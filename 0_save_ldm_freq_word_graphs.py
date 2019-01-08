ad132b5ddf21e5cbbc1905ea5b3e7a91155f6d66
Cai <c.wingfield@lancaster.ac.uk>
2019-01-08 11:08:09 +0000

    Function to save edgelists from N-gram models

--- 0_save_ldm_freq_word_graphs.py
+++ 0_save_ldm_freq_word_graphs.py
@@ -25,10 +25,11 @@ from sklearn.metrics.pairwise import pairwise_distances
 from ldm.core.corpus.indexing import FreqDist, TokenIndex
 from ldm.core.model.base import DistributionalSemanticModel
 from ldm.core.model.count import LogCoOccurrenceCountModel
+from ldm.core.model.ngram import LogNgramModel
 from ldm.core.utils.logging import log_message, date_format
 from ldm.core.utils.maths import DistanceType
 from ldm.preferences.preferences import Preferences as CorpusPreferences
-from model.graph import save_edgelist_from_distance_matrix
+from model.graph import save_edgelist_from_distance_matrix, save_edgelist_from_similarity_matrix
 from model.utils.indexing import list_index_dictionaries
 from preferences import Preferences
 
@@ -43,7 +44,8 @@ def main():
     freq_dist = FreqDist.load(corpus.freq_dist_path)
     token_index = TokenIndex.from_freqdist_ranks(freq_dist)
     distance_type = DistanceType.cosine
-    distributional_model = LogCoOccurrenceCountModel(corpus, window_radius=5, freq_dist=freq_dist)
+    # distributional_model = LogCoOccurrenceCountModel(corpus, window_radius=5, freq_dist=freq_dist)
+    distributional_model = LogNgramModel(corpus, window_radius=5, freq_dist=freq_dist)
 
     # TODO WIP: revert this
     for word_count in [1000]: #Preferences.graph_sizes:
@@ -55,7 +57,13 @@ def main():
         # These dictionaries translate between matrix-row/column indices (after filtering) and token indices within the LDM.
         _, matrix_to_ldm = list_index_dictionaries(filtered_ldm_ids)
 
-        edgelist_filename = f"{distributional_model.name} {distance_type.name} {word_count} words length {length_factor}.edgelist"
+        # Where to save edgelist
+        if distributional_model.model_type.metatype is DistributionalSemanticModel.MetaType.count:
+            edgelist_filename = f"{distributional_model.name} {distance_type.name} {word_count} words length {length_factor}.edgelist"
+        elif distributional_model.model_type.metatype is DistributionalSemanticModel.MetaType.ngram:
+            edgelist_filename = f"{distributional_model.name} {word_count} words length {length_factor}.edgelist"
+        else:
+            raise NotImplementedError()
         edgelist_path = path.join(Preferences.graphs_dir, edgelist_filename)
 
         if path.isfile(edgelist_path):
@@ -65,13 +73,16 @@ def main():
             logger.info("Computing between-word distances")
             distributional_model.train(memory_map=True)
 
+            # Count, predict and n-gram models will be treated differently when building the graph
             if distributional_model.model_type.metatype is DistributionalSemanticModel.MetaType.count:
+                # Convert to csr for slicing, then slice out the rows (i.e. target words) corresponding to the filtered
+                # ids, but leave columns (i.e. context words: data) in-tact for computation of pairwise distances.
                 embedding_matrix = distributional_model.matrix.tocsr()[filtered_ldm_ids, :]
                 distance_matrix = pairwise_distances(embedding_matrix, metric=distance_type.name, n_jobs=-1)
                 # free ram
                 del embedding_matrix
 
-                logger.info(f"Saving graph")
+                logger.info(f"Saving edgelist")
                 save_edgelist_from_distance_matrix(
                     file_path=edgelist_path,
                     distance_matrix=distance_matrix,
@@ -80,11 +91,14 @@ def main():
                 del distance_matrix
 
             elif distributional_model.model_type.metatype is DistributionalSemanticModel.MetaType.ngram:
-                similarity_matrix = distributional_model.underlying_count_model.matrix.to_csr()[filtered_ldm_ids, filtered_ldm_ids]
+                # Convert to csr for slicing rows and to csc for slicing columns
+                similarity_matrix = distributional_model.underlying_count_model.matrix.tocsr()[filtered_ldm_ids, :].tocsc()[:, filtered_ldm_ids]
 
-                # TODO WIP: how to turn similarity into edge length?
-
-                ...
+                logger.info("Saving edgelist")
+                save_edgelist_from_similarity_matrix(
+                    file_path=edgelist_path,
+                    similarity_matrix=similarity_matrix,
+                    length_granularity=length_factor)
 
             else:
                 raise NotImplementedError()
