847e97b3561ec64d848aade467a73c17fe39adac
Cai <c.wingfield@lancaster.ac.uk>
2019-04-03 15:28:52 +0100

    Wider parameter search.

---
+++ jobs/job_2_3/job_2_3_40k_ft0.2_sd8_pmi_ngram.sh
@@ -0,0 +1,26 @@
+# GENERATED CODE, CHANGES WILL BE OVERWRITTEN
+#$ -S /bin/bash
+#$ -q serial
+#$ -N j23_40k_f0.2_s8_pmi_ngram_sa
+#$ -m e
+#$ -M c.wingfield@lancaster.ac.uk
+#$ -l h_vmem=15G
+
+source /etc/profile
+
+echo Job running on compute node `uname -n`
+
+module add anaconda3/2018.12
+
+python3 ../2_3_category_production_ngram_tsa.py \
+           --bailout 20000 \
+           --corpus_name bbc \
+           --firing_threshold 0.2 \
+           --impulse_pruning_threshold 0.05 \
+           --length_factor 10 \
+           --model_name pmi_ngram \
+           --node_decay_factor 0.99 \
+           --radius 5 \
+           --edge_decay_sd_factor 8 \
+           --run_for_ticks 3000 \
+           --words 40000 
--- jobs/job_2_3_builder.py
+++ jobs/job_2_3_builder.py
@@ -21,44 +21,39 @@ graph_sizes = sorted(ram_amount.keys())
 names = []
 
 for model in ["pmi_ngram", "ppmi_ngram"]:
-    for ft, sd in [
-        (0.4,  8), (0.5,  8), (0.6,  8),
-        (0.2, 10), (0.3, 10), (0.4, 10), (0.5, 10),
-        (0.2, 15), (0.3, 15), (0.4, 15), (0.5, 15),
-        (0.2, 20), (0.3, 20), (0.4, 20), (0.5, 20),
-        (0.2, 25), (0.3, 25), (0.4, 25), (0.5, 25),
-    ]:
-        for size in graph_sizes:
-            k = f"{int(size / 1000)}k"
-            name = f"{job_name}_{k}_ft{ft}_sd{sd}_{model}.sh"
-            names.append(name)
-            with open(path.join(job_name, name), mode="w", encoding="utf-8") as job_file:
-                job_file.write(f"# GENERATED CODE, CHANGES WILL BE OVERWRITTEN\n")
-                job_file.write(f"#$ -S /bin/bash\n")
-                job_file.write(f"#$ -q serial\n")
-                job_file.write(f"#$ -N {short_name}_{k}_f{ft}_s{sd}_{model}_sa\n")
-                job_file.write(f"#$ -m e\n")
-                job_file.write(f"#$ -M c.wingfield@lancaster.ac.uk\n")
-                job_file.write(f"#$ -l h_vmem={ram_amount[size]}G\n")
-                job_file.write(f"\n")
-                job_file.write(f"source /etc/profile\n")
-                job_file.write(f"\n")
-                job_file.write(f"echo Job running on compute node `uname -n`\n")
-                job_file.write(f"\n")
-                job_file.write(f"module add anaconda3/2018.12\n")
-                job_file.write(f"\n")
-                job_file.write(f"python3 ../{script_name}.py \\\n")
-                job_file.write(f"           --bailout {int(size / 2)} \\\n")
-                job_file.write(f"           --corpus_name bbc \\\n")
-                job_file.write(f"           --firing_threshold {ft} \\\n")
-                job_file.write(f"           --impulse_pruning_threshold 0.05 \\\n")
-                job_file.write(f"           --length_factor 10 \\\n")
-                job_file.write(f"           --model_name {model} \\\n")
-                job_file.write(f"           --node_decay_factor 0.99 \\\n")
-                job_file.write(f"           --radius 5 \\\n")
-                job_file.write(f"           --edge_decay_sd_factor {sd} \\\n")
-                job_file.write(f"           --run_for_ticks 3000 \\\n")
-                job_file.write(f"           --words {int(size)} \n")
+    for ft in [0.2, 0.3, 0.4, 0.5, 0.6]:
+        for sd in [8, 10, 15, 20, 25]:
+            for size in graph_sizes:
+                k = f"{int(size / 1000)}k"
+                name = f"{job_name}_{k}_ft{ft}_sd{sd}_{model}.sh"
+                names.append(name)
+                with open(path.join(job_name, name), mode="w", encoding="utf-8") as job_file:
+                    job_file.write(f"# GENERATED CODE, CHANGES WILL BE OVERWRITTEN\n")
+                    job_file.write(f"#$ -S /bin/bash\n")
+                    job_file.write(f"#$ -q serial\n")
+                    job_file.write(f"#$ -N {short_name}_{k}_f{ft}_s{sd}_{model}_sa\n")
+                    job_file.write(f"#$ -m e\n")
+                    job_file.write(f"#$ -M c.wingfield@lancaster.ac.uk\n")
+                    job_file.write(f"#$ -l h_vmem={ram_amount[size]}G\n")
+                    job_file.write(f"\n")
+                    job_file.write(f"source /etc/profile\n")
+                    job_file.write(f"\n")
+                    job_file.write(f"echo Job running on compute node `uname -n`\n")
+                    job_file.write(f"\n")
+                    job_file.write(f"module add anaconda3/2018.12\n")
+                    job_file.write(f"\n")
+                    job_file.write(f"python3 ../{script_name}.py \\\n")
+                    job_file.write(f"           --bailout {int(size / 2)} \\\n")
+                    job_file.write(f"           --corpus_name bbc \\\n")
+                    job_file.write(f"           --firing_threshold {ft} \\\n")
+                    job_file.write(f"           --impulse_pruning_threshold 0.05 \\\n")
+                    job_file.write(f"           --length_factor 10 \\\n")
+                    job_file.write(f"           --model_name {model} \\\n")
+                    job_file.write(f"           --node_decay_factor 0.99 \\\n")
+                    job_file.write(f"           --radius 5 \\\n")
+                    job_file.write(f"           --edge_decay_sd_factor {sd} \\\n")
+                    job_file.write(f"           --run_for_ticks 3000 \\\n")
+                    job_file.write(f"           --words {int(size)} \n")
 with open(f"{job_name}_submit_ALL.sh", mode="w", encoding="utf-8") as batch_file:
     batch_file.write(f"# GENERATED CODE, CHANGES WILL BE OVERWRITTEN\n")
     batch_file.write(f"#!/usr/bin/env bash\n")
