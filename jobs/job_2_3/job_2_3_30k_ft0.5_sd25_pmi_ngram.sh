0a12ee3b20f12634bea46f28231e2bf967d8bf17
Cai <c.wingfield@lancaster.ac.uk>
2019-04-03 11:37:47 +0100

    More param search jobs.

---
+++ jobs/job_2_3/job_2_3_30k_ft0.5_sd25_pmi_ngram.sh
@@ -0,0 +1,26 @@
+# GENERATED CODE, CHANGES WILL BE OVERWRITTEN
+#$ -S /bin/bash
+#$ -q serial
+#$ -N j23_30k_f0.5_s25_pmi_ngram_sa
+#$ -m e
+#$ -M c.wingfield@lancaster.ac.uk
+#$ -l h_vmem=12G
+
+source /etc/profile
+
+echo Job running on compute node `uname -n`
+
+module add anaconda3/2018.12
+
+python3 ../2_3_category_production_ngram_tsa.py \
+           --bailout 15000 \
+           --corpus_name bbc \
+           --firing_threshold 0.5 \
+           --impulse_pruning_threshold 0.05 \
+           --length_factor 10 \
+           --model_name pmi_ngram \
+           --node_decay_factor 0.99 \
+           --radius 5 \
+           --edge_decay_sd_factor 25 \
+           --run_for_ticks 3000 \
+           --words 30000 
