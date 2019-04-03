edc1ecc9f21d6e90c866923c50ad0961d38c92f3
Cai <c.wingfield@lancaster.ac.uk>
2019-04-03 11:32:53 +0100

    More param search jobs.

--- jobs/jobs_3_1_param_search.sh
+++ jobs/jobs_3_1_param_search.sh
@@ -37,3 +37,5 @@ python ../3_1_category_production_comparison.py "${DATA_LOC}/${PREFIX} (40,000 w
 python ../3_1_category_production_comparison.py "${DATA_LOC}/${PREFIX} (40,000 words; firing 0.7; sd_factor 4.0 length 100; model [log n-gram (BBC), r=5])"  0.7 &
 python ../3_1_category_production_comparison.py "${DATA_LOC}/${PREFIX} (40,000 words; firing 0.8; sd_factor 5.0 length 100; model [log n-gram (BBC), r=5])"  0.8 &
 python ../3_1_category_production_comparison.py "${DATA_LOC}/${PREFIX} (40,000 words; firing 0.9; sd_factor 6.0 length 100; model [log n-gram (BBC), r=5])"  0.9 &
+python ../3_1_category_production_comparison.py "${DATA_LOC}/${PREFIX} (30,000 words; firing 0.5; sd_factor 15.0; length 10; model [PMI n-gram (BBC), r=5])" 0.5 &
+python ../3_1_category_production_comparison.py "${DATA_LOC}/${PREFIX} (40,000 words; firing 0.5; sd_factor 15.0; length 10; model [PMI n-gram (BBC), r=5])" 0.5 &
