92823c63a4dfd1f79c097dbb1c5e72f4446c98df
Cai <c.wingfield@lancaster.ac.uk>
2019-04-25 15:16:28 +0100

    More 3-3 jobs

--- jobs/jobs_3_3_sensorimotor.sh
+++ jobs/jobs_3_3_sensorimotor.sh
@@ -4,3 +4,6 @@ DATA_LOC="/Volumes/Data/spreading activation model/Model output/sensorimotor com
 FILE_PREFIX="Category production traces [sensorimotor Minkowski-3] length 100"
 
 python ../3_3_category_production_comparison_sensorimotor.py "${DATA_LOC}/${FILE_PREFIX}, pruning at 100 df 0.99; pt 0.05; rft 3000; bailout 3000" &
+python ../3_3_category_production_comparison_sensorimotor.py "${DATA_LOC}/${FILE_PREFIX}, pruning at 150 df 0.99; pt 0.05; rft 3000; bailout 3000" &
+python ../3_3_category_production_comparison_sensorimotor.py "${DATA_LOC}/${FILE_PREFIX}, pruning at 100 df 0.99; pt 0.05; rft 5000; bailout 5000" &
+python ../3_3_category_production_comparison_sensorimotor.py "${DATA_LOC}/${FILE_PREFIX}, pruning at 150 df 0.99; pt 0.05; rft 5000; bailout 5000" &
