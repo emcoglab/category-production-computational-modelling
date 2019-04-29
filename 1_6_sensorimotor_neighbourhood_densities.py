5a9b5d6a55d65969c33a308db2986d66d4a9860a
Cai <c.wingfield@lancaster.ac.uk>
2019-04-29 16:00:03 +0100

    Don't try to do things too often

--- 1_6_sensorimotor_neighbourhood_densities.py
+++ 1_6_sensorimotor_neighbourhood_densities.py
@@ -31,36 +31,32 @@ logger_format = '%(asctime)s | %(levelname)s | %(module)s | %(message)s'
 logger_dateformat = "%Y-%m-%d %H:%M:%S"
 
 
-RADII = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, ]
-
-
 def main(length_factor: int,
-         distance_type_name: str):
+         distance_type_name: str,
+         radius: int):
 
     distance_type = DistanceType.from_name(distance_type_name)
 
     edgelist_filename = f"sensorimotor {distance_type.name} distance length {length_factor}.edgelist"
     edgelist_path = path.join(Preferences.graphs_dir, edgelist_filename)
 
-    for radius in RADII:
+    logger.info(f"Computing neighbourhood distribution for {radius}...")
 
-        logger.info(f"Computing neighbourhood distribution for {radius}...")
+    graph = Graph.load_from_edgelist(edgelist_path, ignore_edges_longer_than=radius)
 
-        graph = Graph.load_from_edgelist(edgelist_path, ignore_edges_longer_than=radius)
+    log_graph_topology(graph)
 
-        log_graph_topology(graph)
+    neighbour_counts = [
+        len(list(graph.neighbourhood(n)))
+        for n in graph.nodes
+    ]
 
-        neighbour_counts = [
-            len(list(graph.neighbourhood(n)))
-            for n in graph.nodes
-        ]
-
-        f = pyplot.figure()
-        distplot(neighbour_counts)
-        f.savefig(path.join(Preferences.figures_dir,
-                            "neighbourhood distributions",
-                            f"points_within_radius_{radius}.png"))
-        pyplot.close(f)
+    f = pyplot.figure()
+    distplot(neighbour_counts)
+    f.savefig(path.join(Preferences.figures_dir,
+                        "neighbourhood distributions",
+                        f"points_within_radius_{radius}.png"))
+    pyplot.close(f)
 
 
 if __name__ == '__main__':
@@ -71,12 +67,13 @@ if __name__ == '__main__':
 
     parser.add_argument("-l", "--length_factor", required=True, type=int)
     parser.add_argument("-d", "--distance_type", required=True, type=str)
-    parser.add_argument("-p", "--prune_length", required=True, type=int)
+    parser.add_argument("-p", "--pruning_length", required=True, type=int)
 
     args = parser.parse_args()
 
     main(length_factor=args.length_factor,
-         distance_type_name=args.distance_type)
+         distance_type_name=args.distance_type,
+         radius=args.pruning_length)
     args = parser.parse_args()
 
     logger.info("Done!")
