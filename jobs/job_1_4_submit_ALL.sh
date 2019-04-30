cb83f236091b570251bef9b9b7601eb80bbdfab0
Cai <c.wingfield@lancaster.ac.uk>
2019-04-30 16:48:36 +0100

    Remove some old unused job scripts

--- jobs/job_1_4_submit_ALL.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-qsub job_1_4_pruned_connectedness_1k_50pc.sh
-qsub job_1_4_pruned_connectedness_3k_50pc.sh
-qsub job_1_4_pruned_connectedness_5k_50pc.sh
-qsub job_1_4_pruned_connectedness_10k_50pc.sh
-qsub job_1_4_pruned_connectedness_15k_50pc.sh
-qsub job_1_4_pruned_connectedness_20k_50pc.sh
-qsub job_1_4_pruned_connectedness_30k_50pc.sh
