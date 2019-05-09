199ecd58fd4129e915a098c0a5ce95aa498b4fae
Cai <c.wingfield@lancaster.ac.uk>
2019-05-09 15:17:15 +0100

    Little test

--- scratch.py
+++ scratch.py
@@ -1,23 +1,38 @@
-from numpy import array
+from numpy import array, Infinity
 
-from model.points_in_space import PointsInSpace
-from sensorimotor_norms.sensorimotor_norms import SensorimotorNorms
+from model.graph import Graph
+from model.temporal_spatial_propagation import TemporalSpatialPropagation
+from model.utils.maths import make_decay_function_exponential_with_decay_factor
 
 
 def main():
-    smn = SensorimotorNorms()
-    words = ["ant", "bee"]
-
-    sm_data = smn.matrix_for_words(words)
-
-    pis = PointsInSpace(
-        data_matrix=array(sm_data)
+    distance_matrix = array([
+        [.0, .3, .6],  # Lion
+        [.3, .0, .4],  # Tiger
+        [.6, .4, .0],  # Stripes
+    ])
+    graph = Graph.from_distance_matrix(
+        distance_matrix=distance_matrix,
+        length_granularity=10,
+    )
+    tsp = TemporalSpatialPropagation(
+        underlying_graph=graph,
+        point_labelling_dictionary={0: "lion", 1: "tiger", 2: "stripes"},
+        buffer_pruning_threshold=.1,
+        node_decay_function=make_decay_function_exponential_with_decay_factor(decay_factor=0.9),
+        activation_cap=Infinity,
     )
 
-    labelling_dictionary = {i: word for i, word in enumerate(words)}
+    tsp.activate_item_with_label("lion", 1)
 
-    print(pis.point_with_idx(0))
-    print(pis.point_with_idx(1))
+    for i in range(1, 16):
+        tsp.tick()
+
+    # WARNING!!!
+    # These numbers not manually verified, just copied from the output for the purposes of refactoring!!!!!
+    print(f"{tsp.activation_of_item_with_label('lion'):.4},    0.2059")
+    print(f"{tsp.activation_of_item_with_label('tiger'):.4},   0.2824")
+    print(f"{tsp.activation_of_item_with_label('stripes'):.4}, 0.3874")
 
 
 if __name__ == '__main__':
