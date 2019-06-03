167bf0b9b61ce4241dc7c8e4ed98cb32bcc8a0c6
Cai <c.wingfield@lancaster.ac.uk>
2019-06-03 10:20:08 +0100

    Support for multiword categories and responses
    Activate all terms of multi-word categories.
    Get the min-ttfa for multi-word responses' constituent words.
    "Multi-word" actually just means multiple-norms required to encompass the CP term.

--- 3_3_category_production_comparison_sensorimotor.py
+++ 3_3_category_production_comparison_sensorimotor.py
@@ -20,7 +20,7 @@ import argparse
 import logging
 import sys
 from os import path
-from typing import Dict, DefaultDict
+from typing import Dict
 
 from numpy import nan, array
 from pandas import DataFrame
@@ -28,6 +28,7 @@ from pandas import DataFrame
 from category_production.category_production import CategoryProduction
 from category_production.category_production import ColNames as CPColNames
 from evaluation.category_production import TTFA, get_model_ttfas_for_category_sensorimotor, save_stats_sensorimotor
+from ldm.corpus.tokenising import modified_word_tokenize
 from ldm.utils.maths import DistanceType, distance
 from preferences import Preferences
 from sensorimotor_norms.exceptions import WordNotInNormsError
@@ -72,7 +73,7 @@ def main(input_results_dir: str, min_first_rank_freq: int = None):
     # Main dataframe holds category production data and model response data
     main_dataframe: DataFrame = category_production.data.copy()
 
-    main_dataframe = add_predictor_columns(main_dataframe, input_results_dir, distance_column=f"{DistanceType.Minkowski3.name} distance")
+    main_dataframe = add_predictor_columns_ttfa_distance(main_dataframe, input_results_dir, distance_column=f"{DistanceType.Minkowski3.name} distance")
 
     # Save item-level data
     per_category_stats_output_path = path.join(Preferences.results_dir,
@@ -110,21 +111,58 @@ def main(input_results_dir: str, min_first_rank_freq: int = None):
                             False, min_first_rank_freq)
 
 
-def add_predictor_columns(main_dataframe, input_results_dir, distance_column):
+def add_predictor_columns_ttfa_distance(main_dataframe, input_results_dir, distance_column):
     """Add columns for TTFA and distance, dropping appropriate rows."""
+
     # Drop precomputed distance measures
     main_dataframe.drop(['Sensorimotor.distance.cosine.additive', 'Linguistic.PPMI'], axis=1, inplace=True)
+
     # Get TTFAs and distances
-    model_ttfas: Dict[str, DefaultDict[str, int]] = dict()  # category -> response -> TTFA
+    model_ttfas: Dict[str, Dict[str, int]] = dict()  # category -> response -> TTFA
     for category in category_production.category_labels_sensorimotor:
         model_ttfas[category] = get_model_ttfas_for_category_sensorimotor(category, input_results_dir)
-    main_dataframe[TTFA] = main_dataframe.apply(
-        lambda row: model_ttfas[row[CPColNames.CategorySensorimotor]][row[CPColNames.ResponseSensorimotor]],
-        axis=1)
+
+    def get_min_ttfa_for_multiword_responses(row) -> int:
+        """
+        Helper function to convert a row in the output into a ttfa when the response is formed either of a single or
+        multiple norms terms.
+        """
+        c = row[CPColNames.CategorySensorimotor]
+        r = row[CPColNames.ResponseSensorimotor]
+
+        # If the category is not found in the dictionary, it was not accessed by the model so no TTFA will be present
+        # for any response.
+        try:
+            # response -> TTFA
+            c_ttfas: Dict[str, int] = model_ttfas[c]
+        except KeyError:
+            return nan
+
+        # If the response was directly found, we can return it
+        if r in c_ttfas:
+            return c_ttfas[r]
+
+        # Otherwise, try to break the response into components and find any one of them
+        else:
+            r_ttfas = [c_ttfas[w]
+                       for w in modified_word_tokenize(r)
+                       if (w not in category_production.ignored_words)
+                       and (w in c_ttfas)]
+
+            # The multi-word response is said to be activated the first time any one of its constituent words are
+            if len(r_ttfas) > 1:
+                return min(r_ttfas)
+            # If none of the constituent words have a ttfa, we have no ttfa for the multiword term
+            else:
+                return nan
+
+    main_dataframe[TTFA] = main_dataframe.apply(get_min_ttfa_for_multiword_responses, axis=1)
     main_dataframe[distance_column] = main_dataframe.apply(get_sensorimotor_distance_minkowski3, axis=1)
+
     # Drop rows corresponding to responses which weren't produced by the model
     main_dataframe = main_dataframe[main_dataframe[TTFA].notnull()]
     main_dataframe = main_dataframe[main_dataframe[distance_column].notnull()]
+
     # Now we can convert TTFAs to ints and distances to floats as there won't be null values
     main_dataframe[TTFA] = main_dataframe[TTFA].astype(int)
     main_dataframe[distance_column] = main_dataframe[distance_column].astype(float)
