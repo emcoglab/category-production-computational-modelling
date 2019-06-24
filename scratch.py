f803b150ee51dfd2a56eb78e6d48d252be5fdc7a
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2019-06-24 15:31:33 +0100

    Fix a bug only present in debugger which can cause 'default_factory' to be listed in a defaultdict.items()

--- scratch.py
+++ scratch.py
@@ -1,3 +1,5 @@
-from model.events import *
+from typing import List
 
-print(BailoutEvent(3, 999))
+list1: List[object] = [1, 2, 3]
+list2: List[object] = ["1", "2", "3"]
+list3: List[object] = list1 + list2
