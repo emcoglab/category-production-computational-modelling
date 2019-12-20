e76219113b860f7e6a6e6938151317fd7e689809
Cai <mail@caiwingfield.com>
2019-12-20 14:40:54 +0000

    Clear out unused code

--- 3_3_category_production_comparison_combined_noninteractive.py
+++ 3_3_category_production_comparison_combined_noninteractive.py
@@ -23,21 +23,17 @@ import sys
 from os import path
 from typing import Optional
 
-from numpy import nan, array
+from numpy import Inf
 from pandas import DataFrame
 
-from category_production.category_production import ColNames as CPColNames, CategoryProduction
-from ldm.utils.maths import DistanceType, distance
-from sensorimotor_norms.exceptions import WordNotInNormsError
 from sensorimotor_norms.sensorimotor_norms import SensorimotorNorms
+from category_production.category_production import ColNames as CPColNames, CategoryProduction
 
+from evaluation.category_production import add_ttfa_column, ModelType, save_hitrate_graphs, \
+    get_model_ttfas_for_category_sensorimotor, get_hitrate_summary_tables, get_model_ttfas_for_category_linguistic, \
+    get_n_words_from_path_linguistic, hitrate_within_sd_of_hitrate_mean_frac, \
+    get_firing_threshold_from_path_linguistic, prepare_category_production_data
 from evaluation.column_names import TTFA
-from evaluation.category_production import add_ttfa_column, ModelType, save_item_level_data, save_hitrate_graphs, \
-    get_model_ttfas_for_category_sensorimotor, get_hitrate_summary_tables, save_model_performance_stats, \
-    get_model_ttfas_for_category_linguistic, get_n_words_from_path_linguistic, hitrate_within_sd_of_hitrate_mean_frac, \
-    get_firing_threshold_from_path_linguistic, prepare_category_production_data, drop_missing_data_to_add_types, \
-    save_hitrate_summary_tables
-from preferences import Preferences
 
 logger = logging.getLogger(__name__)
 logger_format = '%(asctime)s | %(levelname)s | %(module)s | %(message)s'
@@ -46,16 +42,16 @@ logger_dateformat = "%Y-%m-%d %H:%M:%S"
 SN = SensorimotorNorms()
 CP = CategoryProduction()
 
+MODEL_TYPE = ModelType.combined_noninteractive
+
+# The number of members of a category to produce when computing TTFA scale ratios
 N_MEMBERS = 3
 
+# Additional TTFA column names for parts of combined model
 TTFA_LINGUISTIC          = f"{TTFA} linguistic"
 TTFA_SENSORIMOTOR        = f"{TTFA} sensorimotor"
 TTFA_SENSORIMOTOR_SCALED = f"{TTFA} sensorimotor scaled"
 TTFA_COMBINED            = f"{TTFA} combined"
-
-distance_column = f"{DistanceType.Minkowski3.name} distance"
-
-model_type = ModelType.combined_noninteractive
 
 
 def main(input_results_dir_sensorimotor: str,
@@ -79,7 +75,7 @@ def main(input_results_dir_sensorimotor: str,
     # model_identifier = f"{input_results_dir_linguistic.parent.name} {input_results_dir_linguistic.name} — " \
     #                    f"{input_results_dir_sensorimotor.parent.name} {input_results_dir_sensorimotor.name}"
     model_identifier = "combined test"
-    output_dir = f"Category production fit {model_type.name}"
+    output_dir = f"Category production fit {MODEL_TYPE.name}"
 
     if this_linguistic_cat is not None:
         file_suffix = f"({model_identifier}) CAT={this_linguistic_cat}"
@@ -166,40 +162,9 @@ def main(input_results_dir_sensorimotor: str,
 
     # region Process model output
 
-    save_item_level_data(main_data, path.join(Preferences.results_dir, output_dir, f"item-level data {file_suffix}.csv"))
 
-    drop_missing_data_to_add_types(main_data, {TTFA: int})
-
-    save_model_performance_stats(
-        main_data,
-        model_identifier=model_identifier,
-        results_dir=None,
-        # TODO: min-frf
-        min_first_rank_freq=None,
-        hitrate_fit_rpf_hr=hitrate_fit_rpf,
-        hitrate_fit_rmr_hr=hitrate_fit_rmr,
-        model_type=model_type,
-        conscious_access_threshold=this_linguistic_cat,
-    )
-
-    save_hitrate_graphs(hitrates_per_rpf, hitrates_per_rmr, model_type, file_suffix)
 
     # endregion -------------------
-
-
-def add_predictor_column_sensorimotor_distance(main_data):
-    """Mutates `main_data`."""
-    logger.info("Adding distance column")
-    main_data[distance_column] = main_data.apply(get_sensorimotor_distance_minkowski3, axis=1)
-
-
-def get_sensorimotor_distance_minkowski3(row):
-    try:
-        category_vector = array(SN.vector_for_word(row[CPColNames.CategorySensorimotor]))
-        response_vector = array(SN.vector_for_word(row[CPColNames.ResponseSensorimotor]))
-        return distance(category_vector, response_vector, DistanceType.Minkowski3)
-    except WordNotInNormsError:
-        return nan
 
 
 if __name__ == '__main__':
