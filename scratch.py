3812d67d25d4284cfe13847d0b79353fdfe597d6
Cai <mail@caiwingfield.com>
2020-02-14 16:42:42 +0000

    Accessible set looks like a simpler buffer

--- scratch.py
+++ scratch.py
@@ -1,13 +1,29 @@
-from typing import List
+class Base:
+    def __init__(self):
+        self.x = set()
 
 
-def gli() -> List[int]: return [1, 2, 3]
+class Derived(Base):
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+        self.__y = len(self.__x) + 10_000
+
+    @property
+    def y(self):
+        return self.__y
 
 
-def gls() -> List[str]:
-    return ["1", "2", "3"]
+if __name__ == '__main__':
+    b = Base()
+    b.x = set(range(10))
 
-
-list1 = gli()
-list2 = gls()
-list3: List[object] = list1 + list2
+    d = Derived()
+    print(d.y)
+    d.x = set(range(10))
+    print(d.y)
