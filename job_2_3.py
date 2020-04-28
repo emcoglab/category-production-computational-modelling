126326ec0e7d236114b4d2645f0b2bad586f6121
Cai <mail@caiwingfield.com>
2020-04-28 10:48:11 +0100

    Bugfixes

--- job_2_3.py
+++ job_2_3.py
@@ -37,15 +37,15 @@ class Job_2_3(LinguisticPropagationJob):
 
 if __name__ == '__main__':
 
-    graph_size = 40_000
-    bailout = int(graph_size / 2)
+    n_words = 40_000
+    bailout = int(n_words / 2)
     impulse_pruning_threshold = 0.05
     node_decay_factor = 0.99
     model_radius = 5
     corpus_name = "bbc"
 
     specs = [
-        LinguisticPropagationJobSpec(model_name="ppmi_ngram", firing_threshold=0.9, edge_decay_sd=15, impulse_pruning_threshold=impulse_pruning_threshold, node_decay_factor=node_decay_factor, model_radius=model_radius, corpus_name=corpus_name, pruning=None, pruning_type=None, graph_size=graph_size, length_factor=10, run_for_ticks=3_000, bailout=bailout),
+        LinguisticPropagationJobSpec(model_name="ppmi_ngram", firing_threshold=0.9, edge_decay_sd=15, impulse_pruning_threshold=impulse_pruning_threshold, node_decay_factor=node_decay_factor, model_radius=model_radius, corpus_name=corpus_name, pruning=None, pruning_type=None, n_words=n_words, length_factor=10, run_for_ticks=3_000, bailout=bailout),
     ]
 
     for job in [Job_2_3(spec) for spec in specs]:
