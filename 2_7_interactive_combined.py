6bf0ce41d9ba2eab6e95534a36739cca28ed6918
Cai <mail@caiwingfield.com>
2020-04-30 11:39:02 +0100

    Basic model structure

---
+++ 2_7_interactive_combined.py
@@ -0,0 +1,34 @@
+#!/Users/cai/Applications/miniconda3/bin/python
+"""
+===========================
+Interactive combined model script.
+===========================
+
+Dr. Cai Wingfield
+---------------------------
+Embodied Cognition Lab
+Department of Psychology
+University of Lancaster
+c.wingfield@lancaster.ac.uk
+caiwingfield.net
+---------------------------
+2020
+---------------------------
+"""
+
+from model.utils.logging import logger
+
+sensorimotor_to_lingusitic_delay = 30
+linguistic_to_sensorimotor_delay = 10
+
+
+def main():
+
+    pass
+
+
+
+if __name__ == '__main__':
+
+    main()
+    logger.info("Done!")
