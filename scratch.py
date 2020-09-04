cff4d42c0fcb4b3b9bc91e89ebc2c72ddd5cce02
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2020-09-04 10:27:58 +0100

    Improve CP-data coverage checking for both components

--- scratch.py
+++ scratch.py
@@ -1,35 +1,4 @@
-from typing import FrozenSet
+from category_production.category_production import CategoryProduction
 
-
-class Base:
-    def __init__(self, items=None):
-        self.items: FrozenSet = frozenset() if items is None else items
-
-    @property
-    def items(self) -> FrozenSet:
-        print("Base getter")
-        return self.__items
-
-    @items.setter
-    def items(self, value):
-        print("Base setter")
-        self.__items = value
-
-
-class Derived(Base):
-
-    @property
-    def items(self) -> FrozenSet:
-        print("Derived getter")
-        return self.__items
-
-    @items.setter
-    def items(self, value):
-        print("Derived setter")
-        self.__items = value
-
-
-if __name__ == '__main__':
-    d = Derived(frozenset({0, 1, 2}))
-    d.items -= {0}
-    print(d.items)
+cp = CategoryProduction()
+pass
