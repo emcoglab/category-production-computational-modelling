b9d9384b331c68169c33cfb5ad2fee06be705d66
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2021-01-29 16:29:32 +0000

    More efficient graph pruning

--- distances_max_min.py
+++ distances_max_min.py
@@ -28,22 +28,24 @@ from framework.cognitive_model.preferences.preferences import Preferences
 
 def main():
     distance_type = DistanceType.Minkowski3
-    length_factor = 100
-    max_sphere_radius = 150
-    # edgelist_filename = f"sensorimotor for testing only {distance_type.name} distance length {length_factor} pruned {max_sphere_radius}.edgelist"
-    edgelist_filename = f"sensorimotor {distance_type.name} distance length {length_factor}.edgelist"
+    length_factor = 159
+    max_sphere_radius = 1.5
+    edgelist_filename = f"sensorimotor for testing only {distance_type.name} distance length {length_factor} pruned {max_sphere_radius}.edgelist"
     min_length, max_length = numpy.Inf, -numpy.Inf
     min_edges = []
     max_edges = []
     for i, (edge, length) in enumerate(iter_edges_from_edgelist(Path(Preferences.graphs_dir, edgelist_filename))):
         if i % 1_000_000 == 0:
-            logger.info(f"\t{i:,}")
+            logger.info(f"{i:,} edges checked")
         if length <= min_length:
             min_length = length
             min_edges.append(edge)
-        elif length >= max_length:
+            continue
+        if length >= max_length:
             max_length = length
             max_edges.append(edge)
+            continue
+
     logger.info(f"Minimum: {min_edges}: {min_length}")
     logger.info(f"Maximum: {max_edges}: {max_length}")
 
