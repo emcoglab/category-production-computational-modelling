81774362c9b900acba9dfd78081f52dad0170fc4
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2021-02-01 08:52:22 +0000

    More args

--- job_0_3.py
+++ job_0_3.py
@@ -2,21 +2,28 @@ from dataclasses import dataclass
 from typing import List
 
 from framework.cli.job import Job, JobSpec, _SerialisableDict
+from framework.cognitive_model.ldm.utils.maths import DistanceType
 
 
 @dataclass
 class JobSpec_0_3(JobSpec):
+    length_factor: int
+    distance_type: DistanceType
+    use_breng_translation: bool
+
     def _to_dict(self) -> _SerialisableDict:
         return {
             **super()._to_dict(),
             "Length factor": str(self.length_factor),
+            "Distance type": self.distance_type.name,
+            "Use BrEng translation": str(self.use_breng_translation)
         }
 
     @classmethod
     def _from_dict(cls, dictionary: _SerialisableDict):
-        return cls(length_factor=int(dictionary["Length factor"]))
-
-    length_factor: int
+        return cls(length_factor=int(dictionary["Length factor"]),
+                   distance_type=DistanceType.from_name(dictionary["Distance type"]),
+                   use_breng_translation=bool(dictionary["Use BrEng translation"]))
 
     @property
     def cli_args(self) -> List[str]:
@@ -46,4 +53,6 @@ class Job_0_3(Job):
 
 
 if __name__ == '__main__':
-    Job_0_3(spec=JobSpec_0_3(length_factor=100)).submit()
+    Job_0_3(
+        JobSpec_0_3(distance_type=DistanceType.Minkowski3, use_breng_translation=True, length_factor=159)
+    ).submit()
