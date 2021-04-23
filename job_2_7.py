5dda7ce5f4b792fa43ce7d97b91f550d5fad7967
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2021-04-23 11:52:04 +0100

    Option to filter events

--- job_2_7.py
+++ job_2_7.py
@@ -64,4 +64,4 @@ if __name__ == '__main__':
             Path(Path(__file__).parent, "job_specifications/job_interactive_testing.yaml"))
     ]
     for job in jobs:
-        job.run_locally()
+        job.run_locally(extra_arguments=["--filter_events accessible_set"])
